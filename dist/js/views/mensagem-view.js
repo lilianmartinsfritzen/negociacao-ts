9fdb508d26d89122aa7bc8c4638a78063904c037
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-01-02 14:42:31 -0300

    feat: add message view

---
+++ dist/js/views/mensagem-view.js
@@ -0,0 +1,16 @@
+export class MensagemView {
+    constructor(selector) {
+        this.elemento = document.querySelector(selector);
+    }
+    template(model) {
+        return `
+      <p class="alert alert-info">
+        ${model}
+      </p>
+    `;
+    }
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
