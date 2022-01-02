6f96898f2b40d5613760c5e34737fd90749a7d1d
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-01-02 17:18:25 -0300

    refactor: protected template

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -10,7 +10,7 @@ import { View } from "./view.js";
  */
 export class NegociacoesView extends View<Negociacoes> {
 
-  template(model: Negociacoes): string {
+  protected template(model: Negociacoes): string {
     return `
       <table class="table table-hover table-bordered">
         <thead>
