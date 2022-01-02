c3d7b00c9c1bc8632e7812d20f4e4b448d6a6344
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-01-02 19:02:26 -0300

    refactor: add class enumeration

---
+++ app/enums/dias-da-semana.ts
@@ -0,0 +1,14 @@
+/**
+ * Apesar do enum atribuir valores a partir de 0 de forma padrão, é interessante atribuir manualmente
+ * a fim de evitar que qualquer alteração no posicionamento afete a aplicação.
+ */
+
+export enum DiasDaSemana {
+  DOMINGO = 0,
+  SEGUNDA = 1,
+  TERCA = 2,
+  QUARTA = 3,
+  QUINTA = 4,
+  SEXTA = 5,
+  SABADO = 6
+}
---
+++ dist/js/enums/dias-da-semana.js
@@ -0,0 +1,14 @@
+/**
+ * Apesar do enum atribuir valores a partir de 0 de forma padrão, é interessante atribuir manualmente
+ * a fim de evitar que qualquer alteração no posicionamento afete a aplicação.
+ */
+export var DiasDaSemana;
+(function (DiasDaSemana) {
+    DiasDaSemana[DiasDaSemana["DOMINGO"] = 0] = "DOMINGO";
+    DiasDaSemana[DiasDaSemana["SEGUNDA"] = 1] = "SEGUNDA";
+    DiasDaSemana[DiasDaSemana["TERCA"] = 2] = "TERCA";
+    DiasDaSemana[DiasDaSemana["QUARTA"] = 3] = "QUARTA";
+    DiasDaSemana[DiasDaSemana["QUINTA"] = 4] = "QUINTA";
+    DiasDaSemana[DiasDaSemana["SEXTA"] = 5] = "SEXTA";
+    DiasDaSemana[DiasDaSemana["SABADO"] = 6] = "SABADO";
+})(DiasDaSemana || (DiasDaSemana = {}));
