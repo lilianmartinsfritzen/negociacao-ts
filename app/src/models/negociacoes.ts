8b58a9644e245186d494a9dec8f5a148a1f7cc00
Lílian Martins Fritzen <liliancmartinsfritzen@gmail.com>
2022-01-17 23:42:46 -0300

    feat: console.log formatted

--- app/src/models/negociacoes.ts
+++ app/src/models/negociacoes.ts
@@ -13,7 +13,11 @@ export class Negociacoes {
   }
   
   public lista(): readonly Negociacao[] {
-    return this.negociacoes
+    return this.negociacoes;
+  }
+
+  public paraTexto(): string {
+    return JSON.stringify(this.negociacoes, null, 2);
   }
   
 }
